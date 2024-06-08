ffcb8c56a4e47243713cd77a3b1833eddd5e257b
ibrahim-pk <emonnstu14@gmail.com>
2024-06-09 01:57:00 +0600

    change baseurl1

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,6 +7,7 @@ import SixthSection from "./SixthSection";
 import SeventhSection from "./SeventhSection";
 
 import Footer from "../Commmon/Footer/Footer";
+import MetaDecorator from "@/components/Utils/MetaDecorator";
 
 function Home() {
   const [showFooterSearch, setShowFooterSearch] = useState(false);
@@ -16,6 +17,7 @@ function Home() {
 
   return (
     <>
+      <MetaDecorator title="হোম-বার্তালয়২৪" description="অনলাইন ভিত্তিক নিউজ পোর্টাল" baseUrl="https://www.bartaloy24.com/"  imageUrl="https://drive.google.com/file/d/1GAtbDS7Yab4cPibyWLuGYDFXHiFvPiD8/view?usp=drive_link" />
       <Header onSearchButtonClick={onSearchButtonClick} />
       {!showFooterSearch && (
         <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,16 @@ import MetaDecorator from '@/components/Utils/MetaDecorator';
 import HomePage from '../Home/Home'
 import Image from 'next/image';
 import { extractFileId } from '@/components/Utils/FileId';
+import Head from 'next/head';
 export default function Home() {
   // let url="https://drive.google.com/file/d/1GAtbDS7Yab4cPibyWLuGYDFXHiFvPiD8/view?usp=drive_link"
   // let imgUrl=extractFileId(url)
   return (
     <>
-      <MetaDecorator title="হোম-বার্তালয়২৪" description="অনলাইন ভিত্তিক নিউজ পোর্টাল" baseUrl="https://www.bartaloy24.com/"  imageUrl="https://drive.google.com/file/d/1GAtbDS7Yab4cPibyWLuGYDFXHiFvPiD8/view?usp=drive_link" />
        {/* <Image src={imgUrl} alt='' height="100" width="100"/> */}
-       
+       <Head>
+       <meta name="google-site-verification" content="xsk8qagysJtZHM5JN6P1VXaAyb6EDeWBU2wEL99DaWI" />
+       </Head>
       <HomePage />   
     </>
   );
